b67b385f8820d189947d02c7453ed216e3a912d1
Dmitriy Zoteev <zdima4444@yandex.ru>
2025-10-08 11:17:16 +0300

    TEST

--- public_html/check-dbs.sh
+++ public_html/check-dbs.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 echo "Checking database /dbdata/catalog.db..."
-sqlite3 /dbdata/catalog.db ".tables" && echo "Database /dbdata/catalog.db opened successfully" || { echo "Failed to open /dbdata/catalog.db"; exit 1; }
+result=$(sqlite3 /dbdata/catalog.db "SELECT EXISTS (SELECT * FROM sqlite_master WHERE type='table' AND name='catalog');")
+if [ "$result" -eq 1 ]; then
+    echo "Database /dbdata/catalog.db opened successfully"
+else
+    echo "Failed to open /dbdata/catalog.db or table 'catalog' not found"
+    exit 1
+fi
+
 echo "Checking database /dbdata/admins.db..."
-sqlite3 /dbdata/admins.db ".tables" && echo "Database /dbdata/admins.db opened successfully" || { echo "Failed to open /dbdata/admins.db"; exit 1; }
+result=$(sqlite3 /dbdata/admins.db "SELECT EXISTS (SELECT * FROM sqlite_master WHERE type='table' AND name='users');")
+if [ "$result" -eq 1 ]; then
+    echo "Database /dbdata/admins.db opened successfully"
+else
+    echo "Failed to open /dbdata/admins.db or table 'users' not found"
+    exit 1
+fi
+
 exit 0
