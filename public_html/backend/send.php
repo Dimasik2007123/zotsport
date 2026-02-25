cc4be4f83a47d7a5b89d009508ff49c895f5b244
Dmitry <zdima4444@gmail.com>
2026-02-25 06:50:28 +0300

    Edited env

--- public_html/backend/send.php
+++ public_html/backend/send.php
@@ -3,8 +3,9 @@ require '/var/www/html/vendor/autoload.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+/*use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;*/
+use SendGrid\Mail\Mail;
 
 if (isset($_POST['name'])) {$name = $_POST['name'];}
 if (isset($_POST['surname'])) {$surname = $_POST['surname'];}
@@ -15,7 +16,7 @@ $selec = $_POST['connect'];
 $spis = $_POST['product_list'];
 $mes = "Тема: Заказ ЗотСпорт\nИмя: $name\nФамилия: $surname\nТелефон: $phone\nПочта: $email\nАдрес: $city\nСпособ связи: $selec\nСписок товаров: $spis"; //Текст сообщения
 
-$mail = new PHPMailer(True);
+/*$mail = new PHPMailer(True);
 $mail->isSMTP();
 $mail->Host = 'smtp.yandex.ru'; 
 $mail->SMTPAuth = true;
@@ -31,7 +32,23 @@ $mail->CharSet = 'UTF-8';
 $mail->Subject = 'Заказ ЗотСпорт';
 $mail->Body = $mes;
 
-$mail->send();
- 
-echo json_encode((['success' => true, 'message' => 'Заказ успешно отправлен']), JSON_UNESCAPED_UNICODE);
+$mail->send();*/
+
+$email_sendgrid = new Mail();
+$email_sendgrid->setFrom("zdima4444@yandex.ru", "ЗотСпорт");
+$email_sendgrid->setSubject("Заказ ЗотСпорт");
+$email_sendgrid->addTo("zdima4444@gmail.com");
+$email_sendgrid->addContent("text/plain", $mes);
+
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+
+try {
+    $response = $sendgrid->send($email_sendgrid);
+    echo json_encode(['success' => true, 'message' => 'Заказ успешно отправлен'], JSON_UNESCAPED_UNICODE);
+} catch (Exception $e) {
+    echo json_encode(['success' => false, 'message' => 'Ошибка отправки: ' . $e->getMessage()], JSON_UNESCAPED_UNICODE);
+}
+
+exit;
 ?>
+ 
