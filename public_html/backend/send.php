04fd6df4fa3d8fa85cdd3b15e284cc84851e716b
Dmitry <zdima4444@gmail.com>
2026-02-25 16:24:20 +0300

    Edited send

--- public_html/backend/send.php
+++ public_html/backend/send.php
@@ -15,48 +15,6 @@ $selec = $_POST['connect'];
 $spis = $_POST['product_list'];
 $mes = "Тема: Заказ ЗотСпорт\nИмя: $name\nФамилия: $surname\nТелефон: $phone\nПочта: $email\nАдрес: $city\nСпособ связи: $selec\nСписок товаров: $spis";
 
-
-/*$apiKey = getenv('MAILTRAP_API_KEY');
-$postData = [
-    'from' => [
-        'email' => 'hello@demomailtrap.co',
-        'name' => 'ЗотСпорт'
-    ],
-    'to' => [
-        [
-            'email' => 'zdima4444@gmail.com'
-        ]
-    ],
-    'subject' => 'Заказ от ' . $name . ' ' . $surname,
-    'text' => $mes
-];
-
-$ch = curl_init('https://send.api.mailtrap.io/api/send');
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer ' . $apiKey,
-    'Content-Type: application/json'
-]);
-curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$curlError = curl_error($ch);
-
-curl_close($ch);
-
-if ($httpCode >= 200 && $httpCode < 300) {
-    echo json_encode(['success' => true, 'message' => 'Заказ успешно отправлен'], JSON_UNESCAPED_UNICODE);
-} else {
-    $errorMsg = $curlError ?: $response;
-    echo json_encode(['success' => false, 'message' => 'Ошибка отправки: ' . $errorMsg], JSON_UNESCAPED_UNICODE);
-}
-
-?>*/
-
 $mail = new PHPMailer(True);
 $mail->isSMTP();
 $mail->Host = 'smtp.yandex.ru'; 
